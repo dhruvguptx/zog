0ea929321371595889b56504534eebe7144a37d3
Dhruv Gupta <dhruvgupta@outlook.in>
2024-09-05 14:48:31 +0530

    initial

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    alert('Message sent successfully!');
+    setTimeout(function() {
+        window.location.href = 'https://dhruvguptx.github.io/zog/index.html';
+    }, 2000);
+};
